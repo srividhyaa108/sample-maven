832a2d68b6fa18ce68b97a5806b21cd49e780fb3
ubuntu <ubuntu@ip-172-31-35-248.ap-south-1.compute.internal>
2020-08-18 10:21:44 +0000

    git commit -m "first commit"

---
+++ src/main/java/java/mavensample/SAmple_Maven/App.java
@@ -0,0 +1,18 @@
+package java.mavensample.SAmple_Maven;
+
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    @Test
+    public void main1()
+    {
+    	System.out.println("Hi this is first sample");
+    }
+    
+}
---
+++ src/test/java/java/mavensample/SAmple_Maven/AppTest.java
@@ -0,0 +1,17 @@
+package java.mavensample.SAmple_Maven;
+
+import org.testng.annotations.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+ 
+{
+    
+	 @Test
+	    public void main1()
+	    {
+	    	System.out.println("Hi this is first sample");
+	    }
+}
